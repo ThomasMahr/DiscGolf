ac95c878eba29b0637171563fc682a22ef30907f
ThomasMahr <48801885+ThomasMahr@users.noreply.github.com>
2021-03-17 14:53:30 -0400

    GamePlayed model adjustment

--- Models/GamePlayed.cs
+++ Models/GamePlayed.cs
@@ -2,8 +2,10 @@
 {
     public class GamePlayed
     {
+        public int GamePlayedID { get; set; }
         public int GameID { get; set; }
         public int UserID { get; set; }
-        public int score { get; set; }
+        public int CourseID { get; set; }
+        public int Score { get; set; }
     }
 }
